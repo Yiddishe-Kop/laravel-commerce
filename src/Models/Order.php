95a0f4f52eb6f75d44fff5323572ef11f0c06e4b
Yehuda Neufeld <yehuda@yiddishe-kop.com>
2020-10-07 11:02:44 +0300

    models: Order, OrderItem

---
+++ src/Models/Order.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace YiddisheKop\LaravelCommerce\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model {
+
+  protected $guarded = [];
+  protected $timestamps = false;
+
+}
---
+++ src/Models/OrderItem.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace YiddisheKop\LaravelCommerce\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class OrderItem extends Model {
+
+  protected $guarded = [];
+  protected $timestamps = false;
+
+}
