2760e2d7a0c05416fa0b2a9cc901ba6619bc9f3e
Yehuda Neufeld <yehuda@yiddishe-kop.com>
2020-10-11 16:24:05 +0300

    user still has his cart after logout

--- tests/Feature/CartFeatureTest.php
+++ tests/Feature/CartFeatureTest.php
@@ -10,9 +10,19 @@ beforeEach(function () {
     'email' => 'yehuda@yiddishe-kop.com',
   ]);
 
+  $this->anotherUser = UserFactory::new()->create([
+    'name' => 'Aryeh',
+    'email' => 'aryeh@yiddishe-kop.com',
+  ]);
+
   $this->anotherProduct = Product::create([
     'title' => 'BA Hayetzirah',
-    'price' => 777
+    'price' => 222
+  ]);
+
+  $this->aThirdProduct = Product::create([
+    'title' => 'Third Product',
+    'price' => 333
   ]);
 });
 
@@ -26,7 +36,6 @@ test('a user can add items to his cart', function () {
     ]);
 
   $cart = (new Cart)->get();
-
   $cartItem = $cart->items()->first();
   $this->assertEquals(2, $cartItem->quantity);
 
@@ -41,7 +50,7 @@ test('a user can add items to his cart', function () {
 });
 
 /** @test */
-test('a guest can add items to his cart', function () {
+test('a guest can add items to the cart', function () {
   $response = $this
     ->post(route('cart.add'), [
       'product_type' => Product::class,
@@ -72,7 +81,7 @@ test('guest cart gets attached to user when he logs in', function () {
       'quantity' => 3,
     ]);
   $cart = (new Cart)->get();
-  $this->assertEquals(null, $cart->user_id);
+  expect($cart->user_id)->toEqual(null);
 
   $response = $this
     ->actingAs($this->user)
@@ -82,5 +91,38 @@ test('guest cart gets attached to user when he logs in', function () {
       'quantity' => 1,
     ]);
   $cart = (new Cart)->get();
-  $this->assertEquals($this->user->id, $cart->user_id);
+  expect($cart->user_id)->toEqual($this->user->id);
+});
+
+test('user still gets his cart after logging out', function () {
+  $this
+    ->post(route('cart.add'), [
+      'product_type' => Product::class,
+      'product_id' => $this->product->id,
+      'quantity' => 3,
+    ]);
+  $cart = (new Cart)->get();
+  expect($cart->user_id)->toEqual(null);
+  expect($cart->items)->toHaveCount(1);
+
+  $this
+    ->actingAs($this->user)
+    ->post(route('cart.add'), [
+      'product_type' => Product::class,
+      'product_id' => $this->anotherProduct->id,
+      'quantity' => 1,
+    ]);
+  $cart = (new Cart)->get();
+  expect($cart->user_id)->toEqual($this->user->id);
+  expect($cart->items)->toHaveCount(2);
+
+  $this
+    ->post(route('cart.add'), [
+      'product_type' => Product::class,
+      'product_id' => $this->aThirdProduct->id,
+      'quantity' => 7,
+    ]);
+  $cart = (new Cart)->get();
+  expect($cart->user_id)->toEqual($this->user->id);
+  expect($cart->items)->toHaveCount(3);
 });
