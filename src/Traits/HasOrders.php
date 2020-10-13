7fa953969272471b60b7401a54a8c7824ed46476
Yehuda Neufeld <yehuda@yiddishe-kop.com>
2020-10-13 16:35:07 +0300

    imported model, style fix

--- src/Traits/HasOrders.php
+++ src/Traits/HasOrders.php
@@ -3,14 +3,15 @@
 namespace YiddisheKop\LaravelCommerce\Traits;
 
 use YiddisheKop\LaravelCommerce\Models\Order;
+use YiddisheKop\LaravelCommerce\Models\OrderItem;
 
 trait HasOrders {
 
   public function orders() {
     return $this->hasMany(Order::class)->where('status', Order::STATUS_COMPLETED);
   }
-  
-  public function order_items() {
+
+  public function orderItems() {
     return $this->hasManyThrough(OrderItem::class, Order::class);
   }
 
