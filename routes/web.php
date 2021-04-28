f26a53170712a00ccdc2482c2a23f299f324f01b
Yehuda Neufeld <yehuda@yiddishe-kop.com>
2021-04-28 17:37:29 +0300

    fix: add POST webhook

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,4 @@ Route::get('order/{order}/pay', CheckoutController::class)->name('order.pay');
 Route::get('order/{order}/complete', OrderCompleteController::class)->name('order.complete');
 
 Route::get('webhook', WebhookController::class)->name('order.webhook');
+Route::post('webhook', WebhookController::class); // some gateways use POST
