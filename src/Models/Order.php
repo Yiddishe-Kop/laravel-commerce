5863c52c2234eec4d4db0b578fdbd88d1d024c08
Yehuda Neufeld <yehuda@yiddishe-kop.com>
2021-05-11 10:26:32 +0300

    fix: order->timeAgo paid date

--- src/Models/Order.php
+++ src/Models/Order.php
@@ -8,56 +8,57 @@ use YiddisheKop\LaravelCommerce\Traits\HandlesOrders;
 use YiddisheKop\LaravelCommerce\Traits\HandlesCartItems;
 
 class Order extends Model {
-  use HasFactory, HandlesCartItems, HandlesOrders;
+    use HasFactory, HandlesCartItems, HandlesOrders;
 
-  const STATUS_CART = 'cart';
-  const STATUS_PENDING = 'pending';
-  const STATUS_COMPLETED = 'completed';
+    const STATUS_CART = 'cart';
+    const STATUS_PENDING = 'pending';
+    const STATUS_COMPLETED = 'completed';
 
-  protected $guarded = [];
+    protected $guarded = [];
 
-  protected $dates = [
-    'paid_at'
-  ];
+    protected $dates = [
+        'paid_at'
+    ];
 
-  protected $casts = [
-    'is_paid' => 'boolean',
-    'gateway_data' => 'array',
-  ];
+    protected $casts = [
+        'is_paid' => 'boolean',
+        'gateway_data' => 'array',
+    ];
 
-  protected $appends = [
-    'timeAgo',
-  ];
+    protected $appends = [
+        'timeAgo',
+    ];
 
-  public function items() {
-    return $this->hasMany(OrderItem::class);
-  }
+    public function items() {
+        return $this->hasMany(OrderItem::class);
+    }
 
-  public function coupon() {
-    return $this->belongsTo(Coupon::class);
-  }
+    public function coupon() {
+        return $this->belongsTo(Coupon::class);
+    }
 
-  public function user() {
-    return $this->belongsTo(config('user', 'App\\User'));
-  }
+    public function user() {
+        return $this->belongsTo(config('user', 'App\\User'));
+    }
 
-  public function getTimeAgoAttribute() {
-    return $this->created_at->diffForHumans();
-  }
+    public function getTimeAgoAttribute() {
+        if (!$this->paid_at) return null;
+        return $this->paid_at->diffForHumans();
+    }
 
-  public function scopeIsCart($query) {
-    return $query->where('status', self::STATUS_CART);
-  }
+    public function scopeIsCart($query) {
+        return $query->where('status', self::STATUS_CART);
+    }
 
-  public function scopeCompleted($query) {
-    return $query->where('status', self::STATUS_COMPLETED);
-  }
+    public function scopeCompleted($query) {
+        return $query->where('status', self::STATUS_COMPLETED);
+    }
 
-  protected static function boot() {
-    parent::boot();
+    protected static function boot() {
+        parent::boot();
 
-    static::deleting(function (self $cart) {
-      return $cart->items()->delete();
-    });
-  }
+        static::deleting(function (self $cart) {
+            return $cart->items()->delete();
+        });
+    }
 }
