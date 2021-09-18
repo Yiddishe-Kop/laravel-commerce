792ea21ae5644a1463f64e95c71a2c52daf75fc9
Yehuda Neufeld <yehuda@yiddishe-kop.com>
2021-09-18 21:52:55 +0300

    chore: formatting

--- src/Traits/HandlesOrders.php
+++ src/Traits/HandlesOrders.php
@@ -10,10 +10,8 @@ use YiddisheKop\LaravelCommerce\Models\Order;
 
 trait HandlesOrders
 {
-
-    public function setCurrency(string $currency)
+    public function setCurrency(string $currency): self
     {
-
         if ($this->status == Order::STATUS_COMPLETED) {
             throw new OrderAlreadyComplete("Can't change the currency after order has been completed", 1);
         }
@@ -21,13 +19,14 @@ trait HandlesOrders
         $this->update([
             'currency' => $currency
         ]);
+
         $this->calculateTotals();
+
         return $this;
     }
 
     public function markAsCompleted(): self
     {
-
         if (!$this->user_id) {
             throw new OrderNotAssignedToUser("No user assigned to order", 1);
         }
