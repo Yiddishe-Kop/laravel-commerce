083423e03428c6c7214997b684eaefdf5b2816ab
Yehuda Neufeld <yehuda@yiddishe-kop.com>
2022-08-12 17:52:51 +0300

    Use Order model from config

--- src/Http/Controllers/OrderCompleteController.php
+++ src/Http/Controllers/OrderCompleteController.php
@@ -4,20 +4,22 @@ namespace YiddisheKop\LaravelCommerce\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use YiddisheKop\LaravelCommerce\Facades\Cart;
 use YiddisheKop\LaravelCommerce\Models\Order;
 
 class OrderCompleteController extends Controller
 {
-
     public function __invoke(Order $order, Request $request)
     {
-
-        Log::info('Completing payment: ' . $order->gateway);
+        Log::info('Completing payment: '.$order->gateway);
         Log::info($request->input());
+
+        $order = Cart::find($order->id);
 
         // complete the payment
         /** @var \YiddisheKop\LaravelCommerce\Contracts\Gateway $gateway */
         $gateway = new $order->gateway();
+
         return $gateway->complete($order, $request);
     }
 }
